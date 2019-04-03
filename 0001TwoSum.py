b2a982ff0df50aa20ca1cdb28c164fdf02e43ce8
meatchild <meat@yuanlindeMac-mini.local>
2019-04-03 23:58:47 +0800

    time 84ms, mem 14.3MB

---
+++ 0001TwoSum.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+class Solution:
+	def twoSum(self, nums: List[int], target: int) -> List[int]:
+		pass
+		
+if __name__ == "__main__":
+	s = Solution()
+	nums = [2, 7, 11, 15]
+	target = 9
+	result = s.twoSum(nums, target)
+	print(result)
