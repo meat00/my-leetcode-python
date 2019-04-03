c0b234e95b415e19ed73847a76bab51b48796a9e
meatchild <meatchild1981@gmail.com>
2019-04-04 00:01:14 +0800

    time 84ms, mem 14.3MB

--- 0001TwoSum.py
+++ 0001TwoSum.py
@@ -1,12 +1,18 @@
-#!/usr/bin/python
+#!env python3
 
 class Solution:
-	def twoSum(self, nums: List[int], target: int) -> List[int]:
-		pass
-		
+    def twoSum(self, nums, target):
+        m = dict()
+        for i,n in enumerate(nums):
+            ret = target - n
+            # 注意这里需要判断!=None,因为get(ret)可能返回0
+            if m.get(ret) != None:
+                return [i, m[ret]]
+            m[n] = i
+
 if __name__ == "__main__":
-	s = Solution()
-	nums = [2, 7, 11, 15]
-	target = 9
-	result = s.twoSum(nums, target)
-	print(result)
+    s = Solution()
+    nums = [2, 7, 11, 15]
+    target = 9
+    result = s.twoSum(nums, target)
+    print(result)
