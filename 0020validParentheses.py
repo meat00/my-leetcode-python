d39e0e2e2b36278c054bc46a04fc979ce3dbd104
meat00 <meatchild@qq.com>
2019-04-11 21:55:17 +0800

    20, 80ms, 13.2MB

---
+++ 0020validParentheses.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        leftPa  = ['(', '[', '{']
+        rightPa = [')', ']', '}']
+        pre     = list()
+        for c in s:
+            if c in leftPa:
+                pre.append(c)
+            if c in rightPa:
+                if 0 == len(pre):
+                    return False
+                p = pre.pop()
+                if rightPa.index(c) != leftPa.index(p):
+                    return False
+        if len(pre):
+            return False
+        return True
+
+if __name__ == "__main__":
+    s = Solution()
+    tests = [
+            '()',
+            '()[]{}',
+            '(]',
+            '([)]',
+            '{[]}',
+            ']['
+            ]
+    for test in tests:
+        print(s.isValid(test))
