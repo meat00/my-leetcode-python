bb924a6e5c036932ef3c38b7e124353a9dbc2811
meat00 <meatchild@qq.com>
2019-04-13 21:27:41 +0800

    leetcode上测例“[]”出奇怪错误，而本地环境运行正常

---
+++ 0026remove-duplicates-from-sorted-array.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+class Solution:
+    def removeDuplicates(self, nums):
+        if len(nums) < 2:
+            return nums
+        i = 1
+        while i < len(nums):
+            if nums[i-1] == nums[i]:
+                nums.remove(nums[i])
+            else:
+                i += 1
+        return i
+
+
+if __name__ == "__main__":
+    s = Solution()
+    tests = [
+            [],
+            [1, 1, 2],
+            [0, 0, 1, 1, 1, 2, 2, 3, 3, 4],
+            ]
+    for test in tests:
+        ret = s.removeDuplicates(test)
+        print(ret)
+        print(test)
