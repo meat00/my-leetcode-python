18a2275a253750599aa1a76813696b8d608a9893
meat00 <meatchild@qq.com>
2019-04-17 22:19:41 +0800

    28, 72ms, 14.5MB

--- 0028implement-strStr.py
+++ 0028implement-strStr.py
@@ -8,25 +8,41 @@ class Solution:
             return 0
         if len(haystack) < len(needle):
             return -1
-        match_start = 0
-        match_len   = 0
-        for i,s in enumerate(haystack):
-            if s == needle[0]:
-                match_start = i
-                m = match_start
-                n = 0
-                while(haystack[m] == needle[n]):
-                    m += 1
-                    n += 1
-                    if n == len(needle):
-                        return match_start
-                    if m >= len(haystack):
-                        return -1
-        return -1
+        partten_next = self.getNext(needle)
+        i = 0
+        j = 0
+        while i < len(haystack) and j < len(needle):
+            if j == -1 or haystack[i] == needle[j]:
+                i += 1
+                j += 1
+            else:
+                j = partten_next[j]
+        if j == len(needle):
+            return i - j
+        else:
+            return -1
+    def getNext(self, needle):
+        partten_next = list()
+        partten_next.append(-1)
+        i = 0
+        j = -1
+        while i < len(needle) - 1:
+            if j == -1 or needle[j] == needle[i]:
+                i += 1
+                j += 1
+                partten_next.append(j)
+            else:
+                j = partten_next[j]
+            # print("i:%d,j:%d --- " %(i, j), end='')
+            # print(partten_next)
+            # print('*'*10)
+        return partten_next
+
 
 
 if __name__ == "__main__":
     s = Solution()
+    print(s.getNext("abcdabd"))
     print(s.strStr("hello", ""))
     print(s.strStr("hello", "ll"))
     print(s.strStr("abc", "ll"))
