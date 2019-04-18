d61e4003f629540ae8849a33d1cb9ad198d25609
meat00 <meatchild@qq.com>
2019-04-18 23:15:08 +0800

    35增加注释，理解二分查找细节

--- 0035search-insert-position.py
+++ 0035search-insert-position.py
@@ -4,15 +4,15 @@
 class Solution:
     def searchInsert(self, nums, target):
         left, right = 0, len(nums) - 1
-        while left < right:
-            mid = (left + right) // 2
+        while left < right:                 # 退出循环时，left==right
+            mid = (left + right) // 2       # 注意这里的向下取整，只剩2个元素时，mid会等于left
             if nums[mid] < target:
-                left = mid + 1
+                left = mid + 1              # 题目中的插入位置在 小<...<=大，如果大于索引mid处的元素，那么返回必定大于等于mid+1
             elif nums[mid] == target:
                 return mid
             else:
-                right = mid
-        if nums[left] >= target:
+                right = mid                 # right不能为mid-1，可能会造成left>right
+        if nums[left] >= target:            # 此时left==right，相当于判断二分最后的元素与target的大小
             return left
         else:
             return left + 1
