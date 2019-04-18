b9b4ee0523f8a6635d53a123cc8e18c9b7f28c1e
meat00 <meatchild@qq.com>
2019-04-19 00:10:13 +0800

    38, 暴力穷举，leetcode卡掉了

---
+++ 0038count-and-say.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        if n == 1:
+            return "1"
+        pre = "1"
+        for cnt in range(1, n+1):
+            cntQ = list()
+            cntStr = str()
+            for s in pre:
+                if len(cntQ) != 0 and cntQ[0] != s:
+                    cntStr += str(len(cntQ)) + cntQ[0]
+                    cntQ = list()
+                cntQ.append(s)
+            if len(cntQ):
+                cntStr += str(len(cntQ)) + cntQ[0]
+            pre = cntStr
+        return pre
+
+if __name__ == "__main__":
+    s = Solution()
+    for test in range(1, 30):
+        print(s.countAndSay(test))
