00c3d51a9f6724bc4b4038cc340343d09459657e
meat00 <meatchild@qq.com>
2019-05-07 22:06:17 +0800

    70, 84ms, 13MB

--- 0070climbing-stairs.py
+++ 0070climbing-stairs.py
@@ -6,7 +6,12 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         if n <= 2:
             return n
-        return self.climbStairs(n - 1) + self.climbStairs(n - 2)
+        dp = [0 for x in range(n+1)]
+        dp[1] = 1
+        dp[2] = 2
+        for x in range(3, n+1):
+            dp[x] = dp[x-1] + dp[x-2]
+        return dp[n]
 
 if __name__ == "__main__":
     s = Solution()
